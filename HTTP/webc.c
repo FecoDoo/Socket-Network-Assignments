c319bd507741fb664631c26e6d063a54017e2c45
fecodoo <fecodoo@gmail.com>
2019-05-03 22:47:23 +0800

    Without saving

--- HTTP/webc.c
+++ HTTP/webc.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <curl/curl.h>
- 
+
+// 保存并打印内容
+// static size_t writeCallback(void *contents, size_t size, size_t nitems, FILE *file) {
+//   fprintf(stdout,"%s",contents);
+//   return fwrite(contents, size, nitems, file);
+// }
+
+// 打印报头
+// static size_t header(char* b, size_t size, size_t nitems, void *userdata) {
+//   //fprintf(stderr,"%s", b);
+//   //fprintf(stderr,"\n-------------------------------------------------\n");
+//   return 0;
+// }
+
 int main(int argc, char* argv[])
 {
   if(argc != 2) {
@@ -17,29 +30,32 @@ int main(int argc, char* argv[])
   curl_global_init(CURL_GLOBAL_DEFAULT);
  
   curl = curl_easy_init();
+
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, URL);
     
+    // curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+    // curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+
+    
+
     // URL
     long response_code;
     res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-    if (!CURLE_OK == res){
-      printf("Response Error! Response code:$d\n", response_code);
+    if (CURLE_OK != res){
+      fprintf(stderr,"CURLINFO_RESPONSE_CODE Error | Response code:$d\n", response_code);
       exit(1);
+    } else {
+      curl_easy_setopt(curl, CURLOPT_URL, URL);
     }
 
     // HEADER
     long header_size;
     res = curl_easy_getinfo(curl, CURLINFO_HEADER_SIZE,&header_size);
     if(CURLE_OK == res){
-      res = curl_easy_setopt(curl,CURLOPT_HEADER,1L);
-      if (CURLE_OK == res){
-        fprintf(stderr,"\n-------------------------------------------------\n");
-      } else {
-        fprintf(stderr,"Header Error!");
-      }
+      curl_easy_setopt(curl,CURLOPT_HEADER,1L);
+      // curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header);
     } else {
-      printf("Header Error! Respose code: %d\n",res);
+      fprintf(stderr,"CURLINFO_HEADER_SIZE Error! Respose code: %d\n",res);
       exit(1);
     }
 
@@ -47,23 +63,24 @@ int main(int argc, char* argv[])
     long code;
     res = curl_easy_getinfo(curl, CURLINFO_HTTP_CONNECTCODE, &code);
     if(!CURLE_OK == res){
-      printf("Connect Error! Response code: %d\n",res);
+      fprintf(stderr,"CURLINFO_HTTP_CONNECTCODE Error! Response code: %d\n",res);
       exit(1);
     }
 
 
-    /* Perform the request, res will get the return code */
+    // Start performing
     res = curl_easy_perform(curl);
-    /* Check for errors */
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s | RESPONSE: %d\n",curl_easy_strerror(res),res);
-    /* always cleanup */
-    //printf(stdout,"%s",res);
-    fprintf(stderr,"\n-------------------------------------------------\n");
+    
+    // Check for errors
+    if(res != CURLE_OK){
+      fprintf(stderr, "curl_easy_perform() failed: %s | Response code: %d\n",curl_easy_strerror(res),res);
+    }
+
+    // Cleanup
     curl_easy_cleanup(curl);
   }
- 
+  
   curl_global_cleanup();
- 
+
   return 0;
 }
