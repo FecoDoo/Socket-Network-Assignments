16ba11352eaf093296b18f1dab4281f711d638c4
fecodoo <fecodoo@gmail.com>
2019-05-04 20:48:45 +0800

    final

--- HTTP/webc.c
+++ HTTP/webc.c
@@ -16,71 +16,71 @@
 
 int main(int argc, char* argv[])
 {
-  if(argc != 2) {
-    fprintf(stderr, "URL is needed. \n");
-    exit(1);
-  }
+	if(argc != 2) {
+		fprintf(stderr, "URL is needed. \n");
+		exit(1);
+	}
 
-  char * URL = argv[1];
-  //static char * URL = "https://www.baidu.com";
+    char * URL = argv[1];
+    //static char * URL = "https://www.baidu.com";
 
-  CURL *curl;
-  CURLcode res;
- 
-  curl_global_init(CURL_GLOBAL_DEFAULT);
- 
-  curl = curl_easy_init();
+    CURL *curl;
+    CURLcode res;
 
-  if(curl) {
-    
-    // curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
-    // curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+    curl_global_init(CURL_GLOBAL_DEFAULT);
 
-    
+    curl = curl_easy_init();
 
-    // URL
-    long response_code;
-    res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-    if (CURLE_OK != res){
-      fprintf(stderr,"CURLINFO_RESPONSE_CODE Error | Response code:$d\n", response_code);
-      exit(1);
-    } else {
-      curl_easy_setopt(curl, CURLOPT_URL, URL);
+    if(curl) {
+
+        // curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+        // curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+
+
+
+        // URL
+        long response_code;
+        res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+        if (CURLE_OK != res){
+            fprintf(stderr,"CURLINFO_RESPONSE_CODE Error | Response code:$d\n", response_code);
+            exit(1);
+        } else {
+            curl_easy_setopt(curl, CURLOPT_URL, URL);
+        }
+
+        // HEADER
+        long header_size;
+        res = curl_easy_getinfo(curl, CURLINFO_HEADER_SIZE,&header_size);
+        if(CURLE_OK == res){
+            curl_easy_setopt(curl,CURLOPT_HEADER,1L);
+            // curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header);
+        } else {
+            fprintf(stderr,"CURLINFO_HEADER_SIZE Error! Respose code: %d\n",res);
+            exit(1);
+        }
+
+        // CONNECT
+        long code;
+        res = curl_easy_getinfo(curl, CURLINFO_HTTP_CONNECTCODE, &code);
+        if(!CURLE_OK == res){
+            fprintf(stderr,"CURLINFO_HTTP_CONNECTCODE Error! Response code: %d\n",res);
+            exit(1);
+        }
+
+
+        // Start performing
+        res = curl_easy_perform(curl);
+
+        // Check for errors
+        if(res != CURLE_OK){
+            fprintf(stderr, "curl_easy_perform() failed: %s | Response code: %d\n",curl_easy_strerror(res),res);
+        }
+
+        // Cleanup
+        curl_easy_cleanup(curl);
     }
 
-    // HEADER
-    long header_size;
-    res = curl_easy_getinfo(curl, CURLINFO_HEADER_SIZE,&header_size);
-    if(CURLE_OK == res){
-      curl_easy_setopt(curl,CURLOPT_HEADER,1L);
-      // curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, header);
-    } else {
-      fprintf(stderr,"CURLINFO_HEADER_SIZE Error! Respose code: %d\n",res);
-      exit(1);
-    }
+    curl_global_cleanup();
 
-    // CONNECT
-    long code;
-    res = curl_easy_getinfo(curl, CURLINFO_HTTP_CONNECTCODE, &code);
-    if(!CURLE_OK == res){
-      fprintf(stderr,"CURLINFO_HTTP_CONNECTCODE Error! Response code: %d\n",res);
-      exit(1);
-    }
-
-
-    // Start performing
-    res = curl_easy_perform(curl);
-    
-    // Check for errors
-    if(res != CURLE_OK){
-      fprintf(stderr, "curl_easy_perform() failed: %s | Response code: %d\n",curl_easy_strerror(res),res);
-    }
-
-    // Cleanup
-    curl_easy_cleanup(curl);
-  }
-  
-  curl_global_cleanup();
-
-  return 0;
+    return 0;
 }
